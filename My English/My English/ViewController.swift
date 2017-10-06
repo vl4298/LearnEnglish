3913691b4b9377b8e27318a7efa4185efc26b6d7
Van Luu <valu4298@gmail.com>
2017-10-05 20:45:34 -0500

    Initialize project

---
+++ My English/My English/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My English
+//
+//  Created by Van Luu on 10/5/17.
+//  Copyright © 2017 Luu Dinh Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
